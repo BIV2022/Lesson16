63790ad64081e52557df7c5121dfd88d095b7149
Admin <belenki8@gmail.com>
2022-07-17 13:18:47 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String name;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello, friend! Enter your name: ");
+        name = scanner.next();
+        System.out.println("Hello, " + name + "!");
+        
+    }
+}
